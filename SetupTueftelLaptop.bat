880ce54f16e1f74563f1d3c3055c2b8286f9a6ef
tueftelpark-jamil <jamil.koller@2point.ch>
2026-04-14 12:46:54 +0200

    add functionality to also create shortcuts to tinkercad and feedback forms on the desktop

--- SetupTueftelLaptop.bat
+++ SetupTueftelLaptop.bat
@@ -15,18 +15,17 @@ set "DESKTOP_PATH=%USERPROFILE%\Desktop"
 set "ARDUINO_DIR=%LOCALAPPDATA%\Programs\Arduino IDE"
 set "ARDUINO_EXE=%ARDUINO_DIR%\Arduino IDE.exe"
 set "SETUP_EXE=%TEMP%\arduino_setup.exe"
+:: Dauerhafter Ordner fuer die Web-Icons
+set "ICON_DIR=%LOCALAPPDATA%\TueftelPark"
 
 :: --- 1. ARDUINO SCHLIESSEN ---
-echo [1/6] Stelle sicher, dass Arduino IDE geschlossen ist...
-:: Beendet das Programm und alle zugehoerigen Hintergrundprozesse sofort
+echo [1/7] Stelle sicher, dass Arduino IDE geschlossen ist...
 taskkill /F /IM "Arduino IDE.exe" /T >nul 2>&1
-:: Gib Windows 2 Sekunden Zeit, um die Datei-Sperren sauber aufzuheben
 timeout /t 2 >nul
 
-:: --- 2. ARDUINO IDE HERUNTERLADEN (Bypass Winget) ---
+:: --- 2. ARDUINO IDE HERUNTERLADEN ---
 echo.
-echo [2/6] Ermittle aktuellste Arduino IDE Version...
-:: Fragt die offizielle API nach dem Link zur neuesten .exe Datei
+echo [2/7] Ermittle aktuellste Arduino IDE Version...
 for /f "delims=" %%I in ('powershell -command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; $release = Invoke-RestMethod -Uri 'https://api.github.com/repos/arduino/arduino-ide/releases/latest'; ($release.assets | ? { $_.name -match 'Windows_64bit\.exe$' }).browser_download_url"') do set "DOWNLOAD_URL=%%I"
 
 if "!DOWNLOAD_URL!"=="" (
@@ -40,30 +39,52 @@ curl -L -s -o "%SETUP_EXE%" "!DOWNLOAD_URL!"
 
 :: --- 3. INSTALLATION ---
 echo.
-echo [3/6] Installiere Arduino IDE im Hintergrund...
+echo [3/7] Installiere Arduino IDE im Hintergrund...
 echo        Das Installationsfenster bleibt unsichtbar. Bitte kurz warten...
-:: /S ist der Befehl fuer den echten Installer, um ohne Fragen im Hintergrund zu arbeiten
 start /wait "" "%SETUP_EXE%" /S
-
-echo        -^> Installation im Standard-Ordner abgeschlossen!
+echo        -^> Installation abgeschlossen!
 echo.
 
-:: --- 4. DESKTOP-VERKNUEPFUNG SICHERSTELLEN ---
-echo [4/6] Pruefe Arduino-Installation und Desktop-Verknuepfung...
+:: --- 4. DESKTOP-VERKNUEPFUNG ARDUINO ---
+echo [4/7] Pruefe Arduino-Installation und Desktop-Verknuepfung...
 if exist "%ARDUINO_EXE%" (
-    :: Der echte Installer hat es nun ins Startmenue gepackt. Wir legen nur noch den Desktop-Shortcut an.
     powershell -command "$wshell = New-Object -ComObject WScript.Shell; $shortcut = $wshell.CreateShortcut('%DESKTOP_PATH%\Arduino IDE.lnk'); $shortcut.TargetPath = '%ARDUINO_EXE%'; $shortcut.Save()"
     echo        -^> Desktop-Verknuepfung erfolgreich erstellt!
 ) else (
-    echo        -^> [FEHLER] Arduino IDE konnte nicht im Verzeichnis %ARDUINO_DIR% gefunden werden.
+    echo        -^> [FEHLER] Arduino IDE konnte nicht gefunden werden.
 )
+echo.
+
+:: --- 5. WEBSEITEN-VERKNUEPFUNGEN ---
+echo [5/7] Erstelle Webseiten-Verknuepfungen...
+if not exist "%ICON_DIR%" mkdir "%ICON_DIR%"
+
+:: Tinkercad
+echo        -^> Tinkercad
+curl -L -s -o "%ICON_DIR%\tinkercad.ico" "https://www.tinkercad.com/favicon.ico"
+(
+    echo [InternetShortcut]
+    echo URL=https://www.tinkercad.com/
+    echo IconIndex=0
+    echo IconFile=%ICON_DIR%\tinkercad.ico
+) > "%DESKTOP_PATH%\Tinkercad.url"
+
+:: Tuefteln Feedback
+echo        -^> Tuefteln Feedback
+curl -L -s -o "%ICON_DIR%\tuefteln.ico" "https://www.tuefteln.com/favicon.ico"
+(
+    echo [InternetShortcut]
+    echo URL=https://www.tuefteln.com/feedback
+    echo IconIndex=0
+    echo IconFile=%ICON_DIR%\tuefteln.ico
+) > "%DESKTOP_PATH%\Tuefteln Feedback.url"
 echo.
 
 :: Alten temporaeren Entpack-Ordner leeren, falls er noch existiert
 if exist "%TEMP_EXTRACT%" rmdir /S /Q "%TEMP_EXTRACT%"
 
-:: --- 5. SKRIPTE HERUNTERLADEN ---
-echo [5/6] Lade Skripte-Repository von GitHub herunter...
+:: --- 6. SKRIPTE HERUNTERLADEN ---
+echo [6/7] Lade Skripte-Repository von GitHub herunter...
 curl -L -s -o "%TEMP_ZIP%" "%REPO_URL%"
 if %errorlevel% neq 0 (
     echo [FEHLER] Herunterladen fehlgeschlagen. Bitte Internetverbindung pruefen.
@@ -77,16 +98,16 @@ for /R "%TEMP_EXTRACT%" %%F in (*.bat) do (
     copy "%%F" "%DESKTOP_PATH%\" /Y >nul
 )
 
-:: --- 6. AUFRAEUMEN ---
+:: --- 7. AUFRAEUMEN ---
 echo.
-echo [6/6] Raeume temporaere Dateien auf...
+echo [7/7] Raeume temporaere Dateien auf...
 if exist "%SETUP_EXE%" del "%SETUP_EXE%"
 if exist "%TEMP_ZIP%" del "%TEMP_ZIP%"
 if exist "%TEMP_EXTRACT%" rmdir /S /Q "%TEMP_EXTRACT%"
 
 echo.
 echo ========================================================
-echo   Laptop-Setup (Programme & Skripte) erfolgreich!
+echo   Laptop-Setup (Programme, Skripte & Links) erfolgreich!
 echo ========================================================
 echo.
 echo Starte nun automatisch die Library-Installation...
